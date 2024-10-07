fad6555b124b14778270e54d3f1cf72d9b11036f
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-10-07 16:38:37 +0200

    Find a way to stop the api and add it after storing the request data in a variable

--- CargoHub/tests/warehouses_test.py
+++ CargoHub/tests/warehouses_test.py
@@ -1,4 +1,5 @@
 import http.client
+import signal
 import threading
 import json
 import pytest
@@ -10,6 +11,14 @@ def run_api():
      relative_path = 'start-system.bat'
      os.system('cd ..')
      os.system(relative_path)
+
+def kill_api():
+    output = os.popen('tasklist | findstr python').read()
+    for line in output.splitlines():
+        if 'python3.12.exe' in line:
+            pid = int(line.split()[1])
+            os.kill(pid, signal.SIGTERM)
+    print("")
 
 @pytest.fixture
 def setup_teardown():
@@ -33,31 +42,32 @@ def setup_teardown():
         warehouseFile.close()
 
 def test_get_all(setup_teardown):
-        # Arrange
-        headers = {'API_KEY': 'a1b2c3d4e5'}        
-        testWarehouses = '[{"id": 1, "code": "YQZZNL56", "name": "Heemskerk cargo hub", "address": "Karlijndreef 281", "zip": "4002 AS", "city": "Heemskerk", "province": "Friesland", "country": "NL", "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"}, {"id": 2, "code": "GIOMNL90", "name": "Petten longterm hub", "address": "Owenweg 731", "zip": "4615 RB", "city": "Petten", "province": "Noord-Holland", "country": "NL", "contact": {"name": "Maud Adryaens", "phone": "+31836 752702", "email": "nickteunissen@example.com"}, "created_at": "2008-02-22 19:55:39", "updated_at": "2009-08-28 23:15:50"}]'
-        # Add two warehouses to data file to test
-        with open(warehousesPath, 'w') as warehouseFile:
-                warehouseFile.write(testWarehouses)
-                warehouseFile.close()
+    # Arrange
+    headers = {'API_KEY': 'a1b2c3d4e5'}        
+    testWarehouses = '[{"id": 1, "code": "YQZZNL56", "name": "Heemskerk cargo hub", "address": "Karlijndreef 281", "zip": "4002 AS", "city": "Heemskerk", "province": "Friesland", "country": "NL", "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"}, {"id": 2, "code": "GIOMNL90", "name": "Petten longterm hub", "address": "Owenweg 731", "zip": "4615 RB", "city": "Petten", "province": "Noord-Holland", "country": "NL", "contact": {"name": "Maud Adryaens", "phone": "+31836 752702", "email": "nickteunissen@example.com"}, "created_at": "2008-02-22 19:55:39", "updated_at": "2009-08-28 23:15:50"}]'
+    # Add two warehouses to data file to test
+    with open(warehousesPath, 'w') as warehouseFile:
+            warehouseFile.write(testWarehouses)
+            warehouseFile.close()
 
-        threading.Thread(target=run_api).start()
-       
-        # Act
-        # Create a connection to localhost on port 3000
-        connection = http.client.HTTPConnection('localhost', 3000)
+    threading.Thread(target=run_api).start()
+    
+    # Act
+    # Create a connection to localhost on port 3000
+    connection = http.client.HTTPConnection('localhost', 3000)
 
-        # Send a GET request to the specific endpoint
-        connection.request('GET', '/api/v1/warehouses', headers=headers)
+    # Send a GET request to the specific endpoint
+    connection.request('GET', '/api/v1/warehouses', headers=headers)
 
-        # Get the response
-        response = connection.getresponse()
-        data = response.read()
-        warehousesDict = json.loads(data)
-        
-        # Assert
-        assert str(response.status) == '200'
-        assert len(warehousesDict) == 2
+    # Get the response
+    response = connection.getresponse()
+    data = response.read()
+    warehousesDict = json.loads(data)
+    
+    kill_api()
+    # Assert
+    assert str(response.status) == '200'
+    assert len(warehousesDict) == 2
         
 
     
