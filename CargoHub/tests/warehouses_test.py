db2745d685adbd837be4fe96cbed0eaab897184c
bluefire0009 <101553922+bluefire0009@users.noreply.github.com>
2024-10-07 20:18:07 +0200

    Write the test for put and delete endpoints

--- CargoHub/tests/warehouses_test.py
+++ CargoHub/tests/warehouses_test.py
@@ -55,10 +55,10 @@ def test_get_all(setup_teardown):
     # Create a connection to localhost on port 3000
     connection = http.client.HTTPConnection('localhost', 3000)
 
-    # Send two POST requests to the ADD_ONE endpoint
+    # Add the two test warehouses to the database
     for warehouse in testWarehouses:
         json_data = json.dumps(warehouse).encode('utf-8')
-        warehouseRequest = connection.request('POST','/api/v1/warehouses', body=json_data, headers=headers)
+        connection.request('POST','/api/v1/warehouses', body=json_data, headers=headers)
         connection.getresponse().close()
     
     # Send a GET request to the GET_ALL endpoint
@@ -89,10 +89,10 @@ def test_get_one(setup_teardown):
     # Create a connection to localhost on port 3000
     connection = http.client.HTTPConnection('localhost', 3000)
 
-    # Send two POST requests to the ADD_ONE endpoint
+    # Add the two test warehouses to the database
     for warehouse in testWarehouses:
         json_data = json.dumps(warehouse).encode('utf-8')
-        warehouseRequest = connection.request('POST','/api/v1/warehouses', body=json_data, headers=headers)
+        connection.request('POST','/api/v1/warehouses', body=json_data, headers=headers)
         connection.getresponse().close()
     
     # Send a GET request to the GET_SPECIFIC_WAREHOUSE endpoint
@@ -115,3 +115,100 @@ def test_get_one(setup_teardown):
     assert warehouseAfter["province"] == testWarehouses[0]["province"]
     assert warehouseAfter["country"] == testWarehouses[0]["country"]
     assert warehouseAfter["contact"] == testWarehouses[0]["contact"]
+
+def test_put_warehouse(setup_teardown):
+    # Arrange
+    headers = {
+        'API_KEY': 'a1b2c3d4e5', 
+        'Content-Type': 'application/json'
+    }        
+    testWarehouses = [{"id": 1, "code": "YQZZNL56", "name": "Heemskerk cargo hub", "address": "Karlijndreef 281", "zip": "4002 AS", "city": "Heemskerk", "province": "Friesland", "country": "NL", "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"}, 
+                      {"id": 2, "code": "GIOMNL90", "name": "Petten longterm hub", "address": "Owenweg 731", "zip": "4615 RB", "city": "Petten", "province": "Noord-Holland", "country": "NL", "contact": {"name": "Maud Adryaens", "phone": "+31836 752702", "email": "nickteunissen@example.com"}, "created_at": "2008-02-22 19:55:39", "updated_at": "2009-08-28 23:15:50"}]
+    
+    extraWarehouse = {"id": 3, "code": "LIGMAL90", "name": "Petten shortterm hub", "address": "Owenweg 666", "zip": "6420 RB", "city": "Patten", "province": "Zuid-Holland", "country": "DE", "contact": {"name": "Maud Adryaens", "phone": "+31836 752702", "email": "nickteunissen@gmail.com"}, "created_at": "2021-02-22 19:55:39", "updated_at": "2009-08-28 23:15:50"}
+
+    threading.Thread(target=run_api).start()
+    
+    # Act
+    # Create a connection to localhost on port 3000
+    connection = http.client.HTTPConnection('localhost', 3000)
+
+    # Add the two test warehouses to the database
+    for warehouse in testWarehouses:
+        json_data = json.dumps(warehouse).encode('utf-8')
+        connection.request('POST','/api/v1/warehouses', body=json_data, headers=headers)
+        connection.getresponse().close()
+    
+    # Update the first test warehouse with the extraWarehouse
+    json_data = json.dumps(extraWarehouse).encode('utf-8')
+    connection.request('PUT',f'/api/v1/warehouses/{testWarehouses[0]["id"]}', body=json_data, headers=headers)
+    response = connection.getresponse()
+    putStatusCode = response.status
+    response.close()
+
+    # Send a GET request to the GET_SPECIFIC_WAREHOUSE endpoint
+    connection.request('GET', f'/api/v1/warehouses/{extraWarehouse["id"]}', headers=headers)
+
+    # Get the response
+    response = connection.getresponse()
+    data = response.read()
+    warehouseAfter = json.loads(data)
+    
+    kill_api()
+    # Assert
+    assert putStatusCode == 200
+    assert warehouseAfter["code"] == extraWarehouse["code"]
+    assert warehouseAfter["name"] == extraWarehouse["name"]
+    assert warehouseAfter["address"] == extraWarehouse["address"]
+    assert warehouseAfter["zip"] == extraWarehouse["zip"]
+    assert warehouseAfter["city"] == extraWarehouse["city"]
+    assert warehouseAfter["province"] == extraWarehouse["province"]
+    assert warehouseAfter["country"] == extraWarehouse["country"]
+    assert warehouseAfter["contact"] == extraWarehouse["contact"]
+
+def test_delete_warehouse(setup_teardown):
+    # Arrange
+    headers = {
+        'API_KEY': 'a1b2c3d4e5', 
+        'Content-Type': 'application/json'
+    }        
+    testWarehouses = [{"id": 1, "code": "YQZZNL56", "name": "Heemskerk cargo hub", "address": "Karlijndreef 281", "zip": "4002 AS", "city": "Heemskerk", "province": "Friesland", "country": "NL", "contact": {"name": "Fem Keijzer", "phone": "(078) 0013363", "email": "blamore@example.net"}, "created_at": "1983-04-13 04:59:55", "updated_at": "2007-02-08 20:11:00"}, 
+                      {"id": 2, "code": "GIOMNL90", "name": "Petten longterm hub", "address": "Owenweg 731", "zip": "4615 RB", "city": "Petten", "province": "Noord-Holland", "country": "NL", "contact": {"name": "Maud Adryaens", "phone": "+31836 752702", "email": "nickteunissen@example.com"}, "created_at": "2008-02-22 19:55:39", "updated_at": "2009-08-28 23:15:50"}]
+    
+    threading.Thread(target=run_api).start()
+    
+    # Act
+    # Create a connection to localhost on port 3000
+    connection = http.client.HTTPConnection('localhost', 3000)
+
+    # Add the two test warehouses to the database
+    for warehouse in testWarehouses:
+        json_data = json.dumps(warehouse).encode('utf-8')
+        connection.request('POST','/api/v1/warehouses', body=json_data, headers=headers)
+        connection.getresponse().close()
+    
+    # Delete the first warehouse
+    connection.request('DELETE',f'/api/v1/warehouses/{testWarehouses[0]["id"]}', headers=headers)
+    response = connection.getresponse()
+    deleteStatusCode = response.status
+    response.close()
+
+    # Send a GET request to the GET_ALL endpoint
+    connection.request('GET', '/api/v1/warehouses', headers=headers)
+    response = connection.getresponse()
+    data = response.read()
+    warehousesDict = json.loads(data)
+
+    # Try getting the deleted warehouse
+    connection.request('GET', f'/api/v1/warehouses/{testWarehouses[0]["id"]}', headers=headers)
+
+    # Get the response
+    response = connection.getresponse()
+    warehouseAfter = response.read()
+    
+    kill_api()
+    # Assert
+    # "b'null'" Means that no warehouse was found
+    assert str(warehouseAfter) == "b'null'"
+    assert deleteStatusCode == 200
+    assert len(warehousesDict) == 1
