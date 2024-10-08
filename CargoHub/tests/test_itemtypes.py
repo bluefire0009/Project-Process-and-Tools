6ff8bb9708369e07e854936f970d49ba51494081
JohnSpruijt2 <johnspruijt@outlook.com>
2024-10-08 11:23:18 +0200

    Fixed delete and put tests for item_types

--- CargoHub/tests/test_itemtypes.py
+++ CargoHub/tests/test_itemtypes.py
@@ -5,29 +5,29 @@ import json
 
 BASE_URL = "http://localhost:3000/api/v1"
 
-ITEMS_PATH = 'CargoHub/data/items.json'
+ITEM_TYPES_PATH = 'CargoHub/data/item_types.json'
 
 
 @pytest.fixture
 def setup_teardown():
     # Setup code
     # Save the warehouses.json content
-    with open(ITEMS_PATH, 'r') as itemsFile:
-        global itemsBefore
-        itemsBefore = itemsFile.read()
-        itemsFile.close()
+    with open(ITEM_TYPES_PATH, 'r') as itemTypesFile:
+        global itemTypesBefore
+        itemTypesBefore = itemTypesFile.read()
+        itemTypesFile.close()
 
     # Overwrite the content of warehouses.json with empty file
-    with open(ITEMS_PATH, 'w') as itemsFile:
-        itemsFile.write("")
-        itemsFile.close()
+    with open(ITEM_TYPES_PATH, 'w') as itemTypesFile:
+        itemTypesFile.write("")
+        itemTypesFile.close()
 
     yield
     # Teardown code
     # Restore the content of warehouses.json
-    with open(ITEMS_PATH, 'w') as itemsFile:
-        itemsFile.write(itemsBefore)
-        itemsFile.close()
+    with open(ITEM_TYPES_PATH, 'w') as itemTypesFile:
+        itemTypesFile.write(itemTypesBefore)
+        itemTypesFile.close()
 
 
 def test_get_all_item_types():
@@ -37,7 +37,7 @@ def test_get_all_item_types():
     data = response.read()
     result = json.loads(data)
     assert response.status == 200
-    assert len(result) > 100
+    assert len(result) > 50
     assert result[0] == {
         "id": 0,
         "name": "Laptop",
@@ -95,32 +95,19 @@ def test_get_item_types_items():
     }
 
 
-def test_put_item():
+def test_put_item_types():
     jsonData = json.dumps({
-        "uid": "P999999",
-        "code": "mYt79640E",
+        "id": 1,
+        "name": "Desktop",
         "description": "test123",
-        "short_description": "pass",
-        "upc_code": "2541112620796",
-        "model_number": "ZK-417773-PXy",
-        "commodity_code": "z-761-L5A",
-        "item_line": 81,
-        "item_group": 83,
-        "item_type": 74,
-        "unit_purchase_quantity": 3,
-        "unit_order_quantity": 18,
-        "pack_order_quantity": 13,
-        "supplier_id": 10,
-        "supplier_code": "SUP468",
-        "supplier_part_number": "ZH-103509-MLv",
-        "created_at": "2024-10-06 02:30:31",
-        "updated_at": "2024-10-06 02:30:31"
+        "created_at": "1993-07-28 13:43:32",
+        "updated_at": "2022-05-12 08:54:35"
     })
 
     connection = http.client.HTTPConnection('localhost', 3000)
     connection.request(
         'PUT',
-        "/api/v1/items/P999999",
+        "/api/v1/item_types/1",
         headers={
             "API_KEY": "a1b2c3d4e5",
             "Content-Type": "application/json"},
@@ -130,7 +117,8 @@ def test_put_item():
 
     assert response.code == 200
 
-    connection.request('GET', "/api/v1/items/P999999", headers={"API_KEY": "a1b2c3d4e5"})
+    connection.request('GET', "/api/v1/item_types/1", headers={"API_KEY": "a1b2c3d4e5"})
+    time.sleep(1)
     response = connection.getresponse()
     data = response.read()
     result = json.loads(data)
@@ -138,9 +126,9 @@ def test_put_item():
     assert result['description'] == "test123"
 
 
-def test_delete_item():
+def test_delete_item_type():
     connection = http.client.HTTPConnection('localhost', 3000)
-    connection.request('DELETE', "/api/v1/items/P999999", headers={"API_KEY": "a1b2c3d4e5"})
+    connection.request('DELETE', "/api/v1/item_types/2", headers={"API_KEY": "a1b2c3d4e5"})
     response = connection.getresponse()
 
     assert response.code == 200
